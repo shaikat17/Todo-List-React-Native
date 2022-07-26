e4d66de9a4995dfc5e1d6d49d993c6588a9ce23d
shaikat17 <shaikatpal56@gmail.com>
2022-07-27 00:14:52 +0600

    Project Complete. This is only a basic app using basic react

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+
+  const [task, setTask] = useState('')
+  const [taskItem, setTaskItem] = useState([])
+
+  const addTaskHandle = (task) => {
+    if (!task) return
+    Keyboard.dismiss()
+    // console.log(task);
+    setTaskItem(prevTask => {
+      return [task, ...prevTask]
+    })
+    setTask('')
+    // console.log(taskItem);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItem];
+    itemsCopy.splice(index, 1);
+    setTaskItem(itemsCopy)
+  }
+
   return (
     <View style={styles.container}>
       {/* Today's Tasks */}
@@ -11,8 +33,15 @@ export default function App() {
 
         <View style={styles.items}>
           {/* Task List */}
-          <Task text={"Task 1"} />
-          <Task text={"Task 2"} />
+          {
+            taskItem.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} /> 
+                </TouchableOpacity>
+              )
+            })
+          }
         </View>
       </View>
 
@@ -22,9 +51,9 @@ export default function App() {
         style={styles.writeTaskWrapper}
         >
 
-          <TextInput style={styles.input} placeholder={'Write a Task'} />
+          <TextInput style={styles.input} placeholder={'Write a Task'} value={task} onChangeText={ text => setTask(text)} />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => addTaskHandle(task)}>
             <View style={styles.addWrapper}>
               <Text style={styles.addText}>+</Text>
             </View>
@@ -51,13 +80,31 @@ const styles = StyleSheet.create({
     marginTop: 30,
   },
   writeTaskWrapper: {
-
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
   input: {
-
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
   },
   addWrapper: {
-
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
   },
   addText: {
     
